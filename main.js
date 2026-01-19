500c8ab7615bc51272313ff40ef18954e633af4d
Lissandrodev <Luismexico2006@gmail.com>
2026-01-18 23:42:59 -0700

    optimizacion de funciones para guardado de correo

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
 function extraerValor(input) {
-    return input.value;
+  return input.value;
 }
 
-
 function validar() {
-    let emailRegex = /^\w+@\w+\.\w+$/
-    let email = document.querySelector("#email");
-    let error = document.querySelector("span");
+  let emailRegex = /^\w+@\w+\.\w+$/;
+  let email = document.querySelector("#email");
+  let error = document.querySelector("span");
 
-    email.addEventListener("input",()=>{
-        console.log(extraerValor(email));
-    })
-    if (!extraerValor(email)) {
-        error.setAttribute("class","errorstate")
-        email.setAttribute("class","error")
-        setTimeout(()=>{
-            error.removeAttribute("class","errorstate")
-            email.removeAttribute("class","error")
-        },3000)
-        }
-    else {
-        if (emailRegex.test(extraerValor(email))){
-            let emailsuccees = document.querySelector(".succmail");
-            window.location.href="success.html";
-            emailsuccees.textContent = extraerValor(email);
-            console.log(extraerValor(email))
-            console.log("correo valido")
-        }
-        else {
-        alert("Debes cumplir con el formato del correo");
-        console.log("correo invalido");
-        }
+  email.addEventListener("input", () => {
+    console.log(extraerValor(email));
+  });
+  if (!extraerValor(email)) {
+    error.setAttribute("class", "errorstate");
+    email.setAttribute("class", "error");
+    setTimeout(() => {
+      error.removeAttribute("class", "errorstate");
+      email.removeAttribute("class", "error");
+    }, 3000);
+  } else {
+    if (emailRegex.test(extraerValor(email))) {
+        const Guardarcorreo = extraerValor(email);
+        // guardar objetos y strings temporalmente, con clave y valor
+      localStorage.setItem("correo", Guardarcorreo); // con set los guardamos en eset caso string
+      window.location.href = "success.html"; 
     }
+  }
 }
 
 function dismiss() {
-window.location.href="index.html";
+  window.location.href = "index.html";
 }
+document.addEventListener("DOMContentLoaded", () => {
+  let spancorreo = document.querySelector(".succmail");
+  // aqui lo traemos con getitem/ siminlar a un dic
+  const correo = localStorage.getItem("correo");
+  if (correo) {
+    spancorreo.textContent = correo;
+  }
+});
